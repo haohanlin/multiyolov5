154726d318d6548b008b43f413147f0ef1c1a53b
1778829384@qq.com <1778829384@qq.com>
2021-07-22 17:52:13 +0800

    [fix]:添加中文注释，理解

--- detect.py
+++ detect.py
@@ -125,8 +125,12 @@ def detect(save_img=False):
         cudnn.benchmark = True
         
     # Get names and colors
+    # 目标检测的类别名字
     names = model.module.names if hasattr(model, 'module') else model.names
+    print('---whx--- names ',names)
+    # 不同的名字类别对应不同的颜色
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    print('---whx--- colors ',colors)
 
     # Run inference
     if device.type != 'cpu':
@@ -140,12 +144,14 @@ def detect(save_img=False):
             img = img.unsqueeze(0)
 
         # Inference
+        #上下文管理器中执行此操作 不记录操作，将梯度设置为零 
+        # 否则，我们的梯度会记录所有已发生操作的运行记录即loss.backward()将梯度添加到已存储的内容中，而不是替换它们）
         with torch.no_grad():
             t1 = time_synchronized()
             out = model(img, augment=opt.augment)
-            pred = out[0][0]
-            seg = out[1]  # [0]
-        # Apply NMS
+            pred = out[0][0] # 目标检测结果
+            seg = out[1]  # [0]分割结果
+        # Apply NMS 非最大值抑制。 主要作用赛选目标框
             pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
             t2 = time_synchronized()
 
@@ -153,26 +159,29 @@ def detect(save_img=False):
         if classify:
             pred = apply_classifier(pred, modelc, img, im0s)
 
-        # Process detections
+        # Process detections 检测过程
         for i, det in enumerate(pred):  # detections per image
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
             else:
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
+            p = Path(p)  # to Path 路径
+            save_path = str(save_dir / p.name)  # img.jpg生成保存路径
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            s += '%gx%g ' % img.shape[2:]  # print string  
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh 归一化增益whwh
+            print('---whx--- det:',det.shape,im0.shape)
             if len(det):
-                # Rescale boxes from img_size to im0 size
+                # Rescale boxes from img_size to im0 size 从img_size缩放到im0大小  缩放坐标，对应大图片
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                for c in det[:, -1].unique(): #c 表示检测到几个类别分别是什么
+                    print('---whx--- c:',c ) 
+                    n = (det[:, -1] == c).sum()  # detections per class  计算出现的每个类别有几个 n 
+                    print('---whx---- n:',n)
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string 连成字符串
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
@@ -182,23 +191,32 @@ def detect(save_img=False):
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img or view_img:  # Add bbox to image
+                    if save_img or view_img:  # Add bbox to image 把框叠加到图 
+                        # 图片类别标签 和置信度
                         label = f'{names[int(cls)]} {conf:.2f}'
+                        # xyxy 是坐标 im0是原图  label标签  color框的颜色 line_thickness线条粗细
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.5f}s)')
             # seg = seg[0]
+            # 分割 插入
+            print('---whx---',seg.shape)
+            #seg 为[1, 19, 512, 1024] 要采用插值，才能达到原图大小
             seg = F.interpolate(seg, (im0.shape[0], im0.shape[1]), mode='bilinear', align_corners=True)[0]
-
+            #seg 为[19, 1024, 2048] 原图
+            #把图片按分割标签进行绘制颜色
             mask = label2image(seg.max(axis=0)[1].cpu().numpy(), Cityscapes_COLORMAP)[:, :, ::-1]
+            # print('----whx---- seg : ',seg.max(axis=0)[1])
+            # print('----whx---- seg 222: ',seg.max(axis=0)[1].cpu().numpy())
+            print('---whx--- mask:',mask)
             dst = cv2.addWeighted(mask, 0.4, im0, 0.6, 0)
 
             # Stream results
             if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.imshow("segmentation", mask)
-                cv2.imshow("mix", dst)
+                cv2.imshow(str(p), im0) #保存目标检测模型
+                cv2.imshow("segmentation", mask) #保存分割模型
+                cv2.imshow("mix", dst) #保存合成模型
                 cv2.waitKey(0)  # 1 millisecond
 
             if opt.submit:
